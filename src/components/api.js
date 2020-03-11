23803053a78a315ae699f5fa9c64ab1722b6cc5b
Sergey Serpikov <iam32five@gmail.com>
2020-03-11 21:57:28 +0300

    add parseSalaryRange

--- src/components/api.js
+++ src/components/api.js
@@ -1,25 +1,28 @@
 import $ from 'jquery';
 
 export async function loadJobs(pagesQty, searchedPosition) {
-    let pos = (searchedPosition==undefined) ? '' : searchedPosition 
+    let pos = (searchedPosition == undefined) ? '' : searchedPosition
     let arr = []
-    for (let i=1;i<pagesQty;i++)
-    {
-        const url = 'https://ru.jooble.org/работа-'+pos+'/?salary=50000&p='+i
+    for (let i = 1; i < pagesQty; i++) {
+        const url = 'https://ru.jooble.org/работа-' + pos + '/?salary=50000&p=' + i
         const response = await fetch(url);
-        const result = await response.text();
-        const $page = $(result);
-        const $results = $page.find(".result")
+        const html = await response.text();
+        const $html = $(html);
+        const $results = $html.find(".result")
         const jobs = $results.map((index, element) => {
-            const $element = $(element); 
+            const $element = $(element);
             const position = $element.find('.position').text();
-            let salaryRange = $element.find('.salary').text();
-            salaryRange = salaryRange.split(' ').join('');
-            salaryRange = salaryRange.match(/\d+/g).map(salary => parseInt(salary, 10));
-            if (salaryRange==null) salaryRange = [0]
-            return { position, salary: salaryRange };
+            const salaryText = $element.find('.salary').text();
+            return { position, salaryRange: parseSalaryRange(salaryText) };
         })
         arr = arr.concat(jobs.toArray())
     }
     return arr;
+}
+
+function parseSalaryRange(salaryText) {
+    if (salaryText == '') return [0]
+    return salaryText.split(' ').join('')
+    .match(/\d+/g)
+    .map(salary => parseInt(salary, 10))
 }
--- src/components/app/index.js
+++ src/components/app/index.js
@@ -13,33 +13,33 @@ export default async function (root) {
   refreshData()
 }
 
-function drawBasics(){
+function drawBasics() {
 
   const $button = $('#findBtn');
   const $input = $('#vacancyInput')
-  
-  $input.keypress(async function(event){
+
+  $input.keypress(async function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
+    if (keycode == '13') {
       refreshData()
     }
   });
 
-  $button.on('click', async () => { 
+  $button.on('click', async () => {
     refreshData();
   });
 }
 
-async function refreshData(){
+async function refreshData() {
 
   const searchedText = $('#vacancyInput').val()
-  let jobs = await loadJobs(3,searchedText);
+  const jobs = await loadJobs(3, searchedText);
 
   drawJobsSection(jobs)
   drawChart(jobs);
 }
 
-function drawJobsSection(jobs){
+function drawJobsSection(jobs) {
 
   $('#myTable').remove()
   const $table = $('<table id="myTable" class="table">')
@@ -47,8 +47,8 @@ function drawJobsSection(jobs){
   $table.append('<thead class="thead-dark"><tr><th>Вакансия</th><th>Запрплата</th></tr></thead>');
 
   jobs.forEach(job => {
-    const salaryStr = (job.salary.length==1)? job.salary : job.salary[0]+'-'+job.salary[1]
-    $table.append('<tr>'+'<td>'+job.position+'</td>'+'<td>' + salaryStr +' ₽'+'</td>'+'</tr>')
+    const salaryStr = (job.salaryRange.length == 1) ? job.salaryRange : job.salaryRange[0] + '-' + job.salaryRange[1]
+    $table.append('<tr>' + '<td>' + job.position + '</td>' + '<td>' + salaryStr + ' ₽' + '</td>' + '</tr>')
   });
 }
 
@@ -58,49 +58,49 @@ function drawChart(jobs) {
   const $chart = $('<canvas id="myChart" width="400" height="200"></canvas>')
   $root.append($chart)
 
-  const salaryArr = [0,0,0,0]
+  const salaryArr = [0, 0, 0, 0]
   jobs.forEach(job => {
-    let salaryDigits = (job.salary.length==1) ? job.salary[0] : job.salary[1]
-    switch (true){
-      case (salaryDigits<40000) :  salaryArr[0]+=1; break;
-      case (salaryDigits<60000) :  salaryArr[1]+=1; break;
-      case (salaryDigits<80000) :  salaryArr[2]+=1; break;
-      default: salaryArr[3]+=1; break;
+    let salaryDigits = (job.salaryRange.length == 1) ? job.salaryRange[0] : job.salaryRange[1]
+    switch (true) {
+      case (salaryDigits < 40000): salaryArr[0] += 1; break;
+      case (salaryDigits < 60000): salaryArr[1] += 1; break;
+      case (salaryDigits < 80000): salaryArr[2] += 1; break;
+      default: salaryArr[3] += 1; break;
     }
-  }); 
+  });
 
-  const myChart = new Chart($chart , {
-      type: 'bar',
-      data: {
-          labels: ['до 40K ₽', '40K - 60K ₽', '60K - 80K ₽', 'Более 80K ₽'],
-          datasets: [{
-              label: 'Уровень зарплат',
-              data: salaryArr,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
+  const myChart = new Chart($chart, {
+    type: 'bar',
+    data: {
+      labels: ['до 40K ₽', '40K - 60K ₽', '60K - 80K ₽', 'Более 80K ₽'],
+      datasets: [{
+        label: 'Уровень зарплат',
+        data: salaryArr,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
           }
+        }]
       }
-  });  
+    }
+  });
 }
 
 
